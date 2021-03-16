6bbd134446e8a5d76fa29c6999b61de52b3ce4ca
eet349 <51262590+eet349@users.noreply.github.com>
2021-03-16 16:03:35 -0500

    Sets up basic redux boiler plate.

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+	testState: 'test',
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default reducer;
